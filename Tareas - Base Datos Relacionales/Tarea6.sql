b6861f6df918ea4218d0e9d01c8514df21884c26
nalrob <113150880+nalrob@users.noreply.github.com>
2022-10-18 21:41:34 -0500

    m

---
+++ Tareas - Base Datos Relacionales/Tarea6.sql
@@ -0,0 +1,21 @@
+#Tarea 6
+#Funciones de agregación
+
+USE TAREA4 ;
+
+SELECT * from salary ;
+select * from salary where amount >80000 ;
+
+#MEDIA
+Select avg(amount) as "Media" from salary;
+
+#MINIMOS Y MAXIMOS
+select min(amount) as "Valor minimo" from salary;
+select max(amount) as "Valor maximo" from salary;
+
+#CUANTILES
+
+#MODA
+#Para conocer la frecuencia de los Titulos profesionales de los empleados
+SELECT title,count(*) as "Frecuencia" from title group by title order by "Frecuencia" limit 1;
+#Comentarios o Hallagos:
