cde1ea2341f5758ba22dd6051c8813ec7b3ae928
nalrob <113150880+nalrob@users.noreply.github.com>
2022-11-08 22:43:41 -0600

    x

---
+++ Tareas - Base Datos Relacionales/Tarea 8 Procedure.sql
@@ -0,0 +1,23 @@
+
+
+DROP PROCEDURE IF EXISTS cal_r;
+
+DELIMITER $$
+CREATE PROCEDURE cal_r(
+OUT r DECIMAL
+)
+BEGIN
+	select sum(x*y)/(SQRT(sum(x*x))*SQRT(sum(y*y))) from (
+		SELECT X-AVG(X) OVER() x , Y-AVG(Y) OVER() y FROM  muestra
+	) a into r ;
+END
+$$
+
+
+
+
+
+call cal_r(@r);
+select @r;
+
+select * from muestra ;
--- Tareas - Base Datos Relacionales/Tarea 8.sql
+++ Tareas - Base Datos Relacionales/Tarea 8.sql
@@ -1,12 +1,12 @@
-Create database DB;
-use DB;
+Create database testing;
+use testing;
 
-CREATE TABLE DB (
+CREATE TABLE datatable (
 X int,
 Y int 
 );
 
-insert into DB (X,Y) VALUES
+insert into datatable (X,Y) VALUES
 (12500,63),
 (15200,32),
 (14100,26),
@@ -21,47 +21,134 @@ insert into DB (X,Y) VALUES
 (11400,13),
 (17157,22)
 ;
---------
-select * from db;
+
+use testing;
+select X-avg(X) as X1 from datatable;
+select Y-avg(Y) as Y1 from datatable;
+
+
+delimiter $$
+declare X1 as
+
+create function calculate_r (X INT, Y INT)
+RETURN DECIMAL(18,2)
+BEGIN
+RETURN sum(X*Y)/(SQRT(sum(X*X)) *SQRT(sum(Y*Y))) WHERE datatable.X=X
+
+
+
 
 with reg_tmp(x,y) as (
 	SELECT X-AVG(X) OVER() "x-x'", y-AVG(Y) OVER() "y-y'" FROM  DB
 ) 
 select sum(x*y)/(SQRT(sum(x*x)) *SQRT(sum(y*y))) as r from reg_temp;
 
+select r(cast(x as decimal),cast(y as decimal)) from db.db;
 
 -------
+
+
 use db;
 
-create function r (
- x int,
- Y int) 
- returns decimal(18,8)
- begin 
- declare r decimal (18,8);
-    set r = 2 ;
-returns r;
-end
-
-r= sum(X-AVG(X)*y-AVG(Y))/(SQRT(sum( X-AVG(X)* X-AVG(X))) *SQRT(sum(y-AVG(Y)*y-AVG(Y)))) from DB ;
+SET GLOBAL log_bin_trust_function_creators = 1;
 
 
-
- delimiter $$
- create function r (
-   x decimal,
+delimiter $$
+ create function calculate_r (
+   X decimal,
    Y decimal) 
    returns decimal(18,8) 
    begin 
-    declare @x1 decimal(18,8);
-    declare @y1 decimal(18,8);
+    declare r decimal(18,8);
+    declare x1 decimal(18,8);
+    declare y1 decimal(18,8);
     set x1 = X-AVG(X);
     set y1 = y-AVG(Y);
     set r = sum(x1*y1)/(SQRT(sum(x1*x1)) *SQRT(sum(y1*y1)));
+    return calculate_r;
+end$$
+delimiter ;
+
+select x,y,calculate_r from datatable;
+
+
+
+DELIMITER //
+
+
+ 
+
+DELIMITER //
+create function contar ( X INT)
+returns int
+begin
+declare x int;
+select count(*) into X from db;
+return contar;
+end//
+delimiter ;
+
+
+select contar from db ;
+
+
+DELIMITER //
+
+CREATE FUNCTION CalcIncome ( starting_value INT )
+RETURNS INT
+BEGIN
+   DECLARE income INT;
+   SET income = 0;
+   label1: WHILE income <= 3000 DO
+     SET income = income + starting_value;
+   END WHILE label1;
+
+   RETURN income;
+
+END; //
+
+DELIMITER ;
+SELECT CalcIncome (1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+create table muestra as
+select cast(x as decimal) x, cast(y as decimal) y from datatable;
+*/
+
+
+select calculate_test() r from muestra;
+
+
+
+
+
+drop function calculate_test;
+
+delimiter $$
+create function calculate_test () returns decimal
+	begin 
+    declare r decimal;
+    set r = (
+    with rtmp(x,y) as (
+	SELECT X-AVG(X) OVER() "x-x'", y-AVG(Y) OVER() "y-y'" FROM  muestra
+	) 
+    select sum(x*y)/(SQRT(sum(x*x)) *SQRT(sum(y*y))) as r from rtmp);
     return r;
 end$$
 delimiter ;
- 
 
-    
 
+show create function calculate_test;
